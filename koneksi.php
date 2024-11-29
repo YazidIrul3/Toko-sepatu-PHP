ef6e9c4814e6ac3feca6424ba3e5bd7fce2b5672
zidcraft879@gmail.com <zidcraft879@gamil.com>
2024-11-30 06:36:56 +0700

    solve koneksi error

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,6 @@
 $koneksi = mysqli_connect("localhost", "root", "", "db_toko_sepatu");
 
 if (!$koneksi) {
-    die("Koneksi gagal: " . mysql_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
